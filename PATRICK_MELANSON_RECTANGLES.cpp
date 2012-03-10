c85ffd177ffb6ff6f1956b3c7ee234d5e8c99e70
Patrick Melanson <patrickpowns@gmail.com>
2012-03-10 16:19:49 -0500

    rectangles done?

--- PATRICK_MELANSON_RECTANGLES.cpp
+++ PATRICK_MELANSON_RECTANGLES.cpp
@@ -5,37 +5,48 @@ using namespace std;
 
 void printQuad (int w, int h, char cBlock);
 
-int main(){
-    int w = 0;
-    int h = 0;
-    char cBlock;
+int main() {
+	int w = 0;
+	int h = 0;
+	int i = 0;
+	char cBlock;
 
-    cout << "Width of rectangle (integer): ";
-    cin >> w;
-    w--;    //otherwise the rectangle will be one character too long
-    cout << "Height of rectangle (integer): ";
-    cin >> h;
-    h -= 2; //otherwise the rectangle will be two rows too high
-    cout << "Charcater to make up lines (character): ";
-    cin >> cBlock;
+	cout << "Width of rectangle (integer): ";
+	cin >> w;
+	w *= 2; //cell for each monospace fonts is twice as high as it is wide, therefore the rectangle must be printed twice as wide
+	cout << "Height of rectangle (integer): ";
+	cin >> h;
+	cout << "Charcater to make up lines (character): ";
+	cin >> cBlock;
 
-    system("cls");
+	system ("cls");
 
-    printQuad(w, h, cBlock);
+	if (w < 1 || h < 1)
+		cout << "Invalid size";
+	else if (h == 1) {
+		for (i = 0; i < w; i++)
+			cout << cBlock;
+		cout << "\n\nWarning: Cannot display top row, height too small";   //if the rectangle is only 1 unit high (i.e. two cells wide and one cell high) displaying a top row will make the rectangle twice as high.
+	} else
+		printQuad (w, h, cBlock);
 
-    cout << "\n\n";
-    system("pause");
+	cout <<  "\n\n";
+	system ("pause");
 }
 
 void printQuad (int w, int h, char cBlock) {
 
-int i;
+	int i;
 
-for (i=0;i<w;i++)
-    cout << cBlock; //creates top row
-for (i=0;i<h;i++) {
-    cout << cBlock; //prints left border
-    cout << setw(w) << cBlock;  //prints right border
-}
+	for (i = 0; i < w; i++)
+		cout << cBlock; //creates top line
+
+	for (i = 2; i < h; i++)
+		cout << endl << cBlock << setw (w - 1) << cBlock; //prints sides
+
+	cout << endl;
+
+	for (i = 0; i < w; i++)
+		cout << cBlock; //creates base
 
 }
