6895599f7e81fb7cc031bfb95cc5ac27f4729de6
Patrick Melanson <patrickpowns@gmail.com>
2012-03-11 16:10:04 -0400

    yum, enum

--- sandbox.cpp
+++ sandbox.cpp
@@ -2,15 +2,42 @@
 #include <stdlib.h>
 using namespace std;
 
+int pointers() {
+
+	int iNum = 5;
+	int *pPoint;
+	pPoint = &iNum;
+
+	cout << pPoint << endl << &pPoint << endl << *pPoint << endl;
+	iNum = 7;
+	cout << *pPoint << endl;
+
+	return (0);
+}
+
+int enumerate() {
+
+	enum card {ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, JOKER};
+
+	card outCard = QUEEN;
+
+	cout << outCard << endl;
+
+	return (0);
+}
+
 int main() {
 
-    int iNum = 5;
-    int *pPoint;
-    pPoint = &iNum;
-
-    cout << pPoint << endl << &pPoint << endl << *pPoint << endl;
-    iNum = 7;
-    cout << *pPoint << endl;
-
+	cout << "pointers\n\n";
+	pointers();
+	cout << endl;
 	system ("pause");
+
+	cout << "\nenum\n\n";
+	enumerate();
+	cout << endl;
+	system ("pause");
+
+
+	return (0);
 }
