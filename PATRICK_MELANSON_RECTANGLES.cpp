168787008ee6575d29b02d1a56b75def4350b13f
Patrick Melanson <patrickpowns@gmail.com>
2012-03-11 11:57:34 -0400

    added x/y, finished?

--- PATRICK_MELANSON_RECTANGLES.cpp
+++ PATRICK_MELANSON_RECTANGLES.cpp
@@ -3,50 +3,66 @@
 #include <iomanip>
 using namespace std;
 
-void printQuad (int w, int h, char cBlock);
+void printQuad (int w, int h, int x, int y, char cBlock);
 
 int main() {
 	int w = 0;
 	int h = 0;
-	int i = 0;
+	int x = 0;
+	int y = 0;
 	char cBlock;
+
+	int i = 0;
 
 	cout << "Width of rectangle (integer): ";
 	cin >> w;
 	w *= 2; //cell for each monospace fonts is twice as high as it is wide, therefore the rectangle must be printed twice as wide
+
 	cout << "Height of rectangle (integer): ";
 	cin >> h;
+
+	cout << "X offset of top left corner (integer): ";
+	cin >> x;
+	x *= 2;
+	if (x == 2) //due to the nature of setw(), an x value of 2 will only offset the rectangle by 1
+		x++;
+
+	cout << "Y offset of top left corner (integer): ";
+	cin >> y;
+
 	cout << "Charcater to make up lines (character): ";
 	cin >> cBlock;
 
 	system ("cls");
 
-	if (w < 1 || h < 1)
-		cout << "Invalid size";
-	else if (h == 1) {
-		for (i = 0; i < w; i++)
-			cout << cBlock;
-		cout << "\n\nWarning: Cannot display top row, height too small";   //if the rectangle is only 1 unit high (i.e. two cells wide and one cell high) displaying a top row will make the rectangle twice as high.
-	} else
-		printQuad (w, h, cBlock);
+
+	if (w < 1 || h < 1 || x < 0 || y < 0)
+		cout << "Values are invalid";
+	else
+		printQuad (w, h, x, y, cBlock);
 
 	cout <<  "\n\n";
 	system ("pause");
 }
 
-void printQuad (int w, int h, char cBlock) {
+void printQuad (int w, int h, int x, int y, char cBlock) {
 
 	int i;
 
-	for (i = 0; i < w; i++)
-		cout << cBlock; //creates top line
+	for (i = 0; i < y; i++)     //offsets down by y
+		cout << endl;
 
-	for (i = 2; i < h; i++)
-		cout << endl << cBlock << setw (w - 1) << cBlock; //prints sides
+	if (h > 1) {
+		cout << setw (x);           //offsets to right by x
+		for (i = 0; i < w; i++)     //prints top
+			cout << cBlock;
+	}
 
-	cout << endl;
+	for (i = 2; i < h; i++)     //prints sides, offset to right by x
+		cout << setw (x) << endl << cBlock << setw (w - 1) << cBlock;
 
-	for (i = 0; i < w; i++)
-		cout << cBlock; //creates base
+	cout << endl << setw (x);   //offsets to right by x
+	for (i = 0; i < w; i++)     //prints bottom
+		cout << cBlock;
 
 }
