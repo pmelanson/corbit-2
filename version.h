ed91f39b980d67d4b5c89e3e461f06f5061fa03c
Patrick Melanson <patrickpowns@gmail.com>
2012-04-21 23:48:59 -0400

    added vectors completely, working on vector polymorphism

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "11";
+	static const char DATE[] = "21";
 	static const char MONTH[] = "04";
 	static const char YEAR[] = "2012";
 	static const char UBUNTU_VERSION_STYLE[] = "12.04";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 0;
-	static const long BUILD = 15;
-	static const long REVISION = 99;
+	static const long BUILD = 16;
+	static const long REVISION = 104;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 1389;
-	#define RC_FILEVERSION 1,0,15,99
-	#define RC_FILEVERSION_STRING "1, 0, 15, 99\0"
-	static const char FULLVERSION_STRING[] = "1.0.15.99";
+	static const long BUILDS_COUNT = 1521;
+	#define RC_FILEVERSION 1,0,16,104
+	#define RC_FILEVERSION_STRING "1, 0, 16, 104\0"
+	static const char FULLVERSION_STRING[] = "1.0.16.104";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;
