58ae319bede080c92a2d4b18afc04e3d0d10113e
Patrick Melanson <patrickpowns@gmail.com>
2012-05-10 15:00:23 -0400

    added realistic planet values, and colors, yay

--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "25";
-	static const char MONTH[] = "04";
+	static const char DATE[] = "10";
+	static const char MONTH[] = "05";
 	static const char YEAR[] = "2012";
-	static const char UBUNTU_VERSION_STYLE[] = "12.04";
+	static const char UBUNTU_VERSION_STYLE[] = "12.05";
 	
 	//Software Status
 	static const char STATUS[] = "Alpha";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 0;
-	static const long BUILD = 17;
-	static const long REVISION = 111;
+	static const long BUILD = 18;
+	static const long REVISION = 119;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 1560;
-	#define RC_FILEVERSION 1,0,17,111
-	#define RC_FILEVERSION_STRING "1, 0, 17, 111\0"
-	static const char FULLVERSION_STRING[] = "1.0.17.111";
+	static const long BUILDS_COUNT = 1588;
+	#define RC_FILEVERSION 1,0,18,119
+	#define RC_FILEVERSION_STRING "1, 0, 18, 119\0"
+	static const char FULLVERSION_STRING[] = "1.0.18.119";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;
