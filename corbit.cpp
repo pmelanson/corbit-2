579e794e78a736586df84175bb288458998956ba
Patrick Melanson <patrickpowns@gmail.com>
2012-05-17 13:40:48 -0400

    tidyin up code

--- corbit.cpp
+++ corbit.cpp
@@ -1,6 +1,13 @@
 /*******************
 
-CORBIT Program
+ .d8888b.                   888      d8b 888    
+d88P  Y88b                  888      Y8P 888    
+888    888                  888          888    
+888         .d88b.  888d888 88888b.  888 888888 
+888        d88""88b 888P"   888 "88b 888 888    
+888    888 888  888 888     888  888 888 888    
+Y88b  d88P Y88..88P 888     888 d88P 888 Y88b.  
+ "Y8888P"   "Y88P"  888     88888P"  888  "Y888 
 
 Designed by Patrick Melanson
 
@@ -28,7 +35,7 @@ move camera    zoom in/out
      SHIP CONTROLS
 
 W/S
-increase/decrease throttle
+throttle up/down
 
 A/D            ENTER
 spin ccw/cw    set engines to 100
@@ -89,17 +96,6 @@ Isn't that an awesome license? I like it.
 using namespace std;
 
 //globals
-<<<<<<< HEAD
-//const unsigned short int screenWidth = 1280;  //my computer's resolution
-const unsigned short int screenWidth = 1144;    //school resolution
-//const unsigned short int screenHeight = 980;  //my computer's resolution
-const unsigned short int screenHeight = 830;    //school resolution
-
-const double zoomMagnitude = 5;  //when zooming out, actual zoom level = camera.zoom ^ zoomMagnitude, therefore is an exponential zoom
-const float zoomStep = 0.1; //rate at which cameras zoom out
-const unsigned short int maxZoom = 30;
-const unsigned short int minZoom = 0.01;
-=======
 const unsigned short int screenWidth = 1280;  //my computer's resolution
 //const unsigned short int screenWidth = 1144;    //school resolution
 const unsigned short int screenHeight = 980;  //my computer's resolution
@@ -108,7 +104,6 @@ const unsigned short int screenHeight = 980;  //my computer's resolution
 /*const int zoomMagnitude = 3;  //when zooming out, actual zoom level = camera.zoomLevel ^ zoomMagnitude, therefore is an exponential zoom
 const float zoomStep = 0.05; //rate at which cameras zoom out
 const unsigned short int maxZoom = 30;  //the smaller this is, the further you can zoom in
->>>>>>> 303d7b31551601a273243cfe312ef018f4fe2d71
 const unsigned short int panSpeed = 2;
 const double minZoom = 1e-10; //the smaller this is, the farther you can zoom out
 */
@@ -128,10 +123,8 @@ const unsigned short int gridSpace = 50;
 
 //prototypes
 void timeStep();
-void drawBuffer();
-
 void input();
-
+void drawBuffer();
 void debug();
 void gravitate();
 void drawGrid();
@@ -139,19 +132,6 @@ void detectCollision();
 void drawHUD();
 
 //beginning of class declarations
-<<<<<<< HEAD
-struct viewpoint {
-
-    long double x;
-    long double y;
-    double zoom;
-    long double actualZoom();
-    void shift();
-    struct entity *target;
-    struct entity *reference;
-    void autoZoom();
-    bool track;
-=======
 class viewpoint {
 
 	const int zoomMagnitude;  //when zooming out, actual zoom level = camera.zoom ^ zoomMagnitude, therefore is an exponential zoom
@@ -180,7 +160,6 @@ public:
 	viewpoint (const int _zoomMagnitude, const float _zoomStep, const unsigned short int _maxZoom, const double _minZoom, const unsigned short int _panSpeed) :
 		zoomMagnitude (_zoomMagnitude), zoomStep (_zoomStep), maxZoom (_maxZoom), minZoom (_minZoom), panSpeed (_panSpeed)
 	{}
->>>>>>> 303d7b31551601a273243cfe312ef018f4fe2d71
 };
 
 struct display {
@@ -221,8 +200,8 @@ struct entity { //stores data about any physical entity, such as mass and radius
 
 struct solarBody : entity {   //stores information about an astronomical body, in addition to information already stored by an entity
 
-	unsigned int atmosphereHeight;
-	unsigned int atmosphereDrag;
+	unsigned short int atmosphereHeight;
+	unsigned short int atmosphereDrag;
 	unsigned int atmosphereColour;
 
 	void draw();
@@ -253,163 +232,6 @@ vector <solarBody*> body;
 
 int main () {
 
-<<<<<<< HEAD
-    //allegro initializations
-    allegro_init();
-    install_keyboard();
-    set_color_depth (desktop_color_depth() );
-    set_gfx_mode (GFX_AUTODETECT_WINDOWED, screenWidth, screenHeight, 0, 0);
-
-    LOCK_VARIABLE (timer);
-    LOCK_FUNCTION (timestep);
-    install_int_ex (timeStep, BPS_TO_TIMER (frameRate) );
-
-    //bitmap initializations
-    buffer = create_bitmap (SCREEN_W, SCREEN_H);
-
-    //data initializations
-
-    //looping variables, and declaring blank
-    unsigned short int n;
-
-    for (n = 0; n < BODYMAX; n++)
-        body.push_back ( new solarBody() );
-
-    vector<ship*>::iterator spaceship;
-    vector<solarBody*>::iterator rock;
-
-    for (rock = body.begin(); rock != body.end(); ++rock) {
-        (*rock)->name = "Blank";
-        (*rock)->Vx = 0;
-        (*rock)->Vy = 0;
-        (*rock)->radius = 100;
-        (*rock)->mass = 6000;
-        (*rock)->fillColour = makecol (100, 100, 100);
-        (*rock)->atmosphereColour = makecol (110, 110, 110);
-        (*rock)->atmosphereHeight = 5;
-        (*rock)->x = 1000;
-        (*rock)->y = 0;
-    }
-
-    body[MERCURY]->name = "Mercury";
-    body[MERCURY]->x = 0.466697 * AU;
-    body[MERCURY]->y = 0;
-    body[MERCURY]->Vx = 0;
-    body[MERCURY]->Vy = -47.87e3;
-    body[MERCURY]->mass = 3.301e20;
-    body[MERCURY]->radius = 2.42e9;
-    body[MERCURY]->fillColour = makecol (169, 169, 169);
-    body[MERCURY]->atmosphereColour = makecol (100, 169, 169);
-    body[MERCURY]->atmosphereHeight = 7;
-
-    body[VENUS]->name = "Venus";
-    body[VENUS]->x = 0.72823128 * AU;
-    body[VENUS]->y = 0;
-    body[VENUS]->Vx = 0;
-    body[VENUS]->Vy = -35.02e3;
-    body[VENUS]->mass = 4.869e24;
-    body[VENUS]->radius = 6.085e6;
-    body[VENUS]->fillColour = makecol (250, 235, 215);
-    body[VENUS]->atmosphereColour = makecol (250, 200, 215);
-    body[VENUS]->atmosphereHeight = 7;
-
-    body[EARTH]->name = "Earth";
-    body[EARTH]->x = 1.01671388 * AU;
-    body[EARTH]->y = 0;
-    body[EARTH]->Vx = 0;
-    body[EARTH]->Vy = -29.78e3;
-    body[EARTH]->mass = 5.978e24;
-    body[EARTH]->radius = pow (5.0, 8) * 6 * 2.71828182845904523536028747135266249775724709369995;    //[within actual variation]
-    body[EARTH]->fillColour = makecol (34, 139, 34);
-    body[EARTH]->atmosphereColour = makecol (65, 105, 225);
-    body[EARTH]->atmosphereHeight = 7;
-
-    body[MARS]->name = "Mars";
-    body[MARS]->x = 1.665861 * AU;
-    body[MARS]->y = 0;
-    body[MARS]->Vx = 0;
-    body[MARS]->Vy = -24.077e3;
-    body[MARS]->mass = 6.420e23;
-    body[MARS]->radius = 3.375e6;
-    body[MARS]->fillColour = makecol (255, 164, 96);
-    body[MARS]->atmosphereColour = makecol (200, 164, 96);
-    body[MARS]->atmosphereHeight = 7;
-
-    body[JUPITER]->name = "Jupiter";
-    body[JUPITER]->x = 5.458104 * AU;
-    body[JUPITER]->y = 0;
-    body[JUPITER]->Vx = 0;
-    body[JUPITER]->Vy = -13.07e3;
-    body[JUPITER]->mass = 1.899e27;
-    body[JUPITER]->radius = 7.14e7;
-    body[JUPITER]->fillColour = makecol (240, 184, 135);
-    body[JUPITER]->atmosphereColour = makecol (240, 184, 100);
-    body[JUPITER]->atmosphereHeight = 7;
-
-    body[SATURN]->name = "Saturn";
-    body[SATURN]->x = 10.11595804 * AU;
-    body[SATURN]->y = 0;
-    body[SATURN]->Vx = 0;
-    body[SATURN]->Vy = -9.69e3;
-    body[SATURN]->mass = 5.685e26;
-    body[SATURN]->radius = 6.7;
-    body[SATURN]->fillColour = makecol (240, 230, 140);
-    body[SATURN]->atmosphereColour = makecol (240, 200, 140);
-    body[SATURN]->atmosphereHeight = 7;
-
-    body[URANUS]->name = "Uranus";
-    body[URANUS]->x = 20.08330526 * AU;
-    body[URANUS]->y = 0;
-    body[URANUS]->Vx = 0;
-    body[URANUS]->Vy = -6.81e3;
-    body[URANUS]->mass = 8.686e25;
-    body[URANUS]->radius = 2.36e7;
-    body[URANUS]->fillColour = makecol (176, 224, 230);
-    body[URANUS]->atmosphereColour = makecol (100, 224, 230);
-    body[URANUS]->atmosphereHeight = 7;
-
-    body[NEPTUNE]->name = "Neptune";
-    body[NEPTUNE]->x = 30.44125206 * AU;
-    body[NEPTUNE]->y = 0;
-    body[NEPTUNE]->Vx = 0;
-    body[NEPTUNE]->Vy = -5.43e3;
-    body[NEPTUNE]->mass = 1.025e26;
-    body[NEPTUNE]->radius = 2.23e7;
-    body[NEPTUNE]->fillColour = makecol (30, 144, 255);
-    body[NEPTUNE]->atmosphereColour = makecol (30, 100, 255);
-    body[NEPTUNE]->atmosphereHeight = 10;
-
-    body[PLUTO]->name = "Pluto";
-    body[PLUTO]->x = 48.871 * AU;
-    body[PLUTO]->y = 0;
-    body[PLUTO]->Vx = 0;
-    body[PLUTO]->Vy = -4.666e3;
-    body[PLUTO]->mass = 5.0e23;
-    body[PLUTO]->radius = 3.0e6;
-    body[PLUTO]->fillColour = makecol (222, 184, 135);
-    body[PLUTO]->atmosphereColour = makecol (256, 235, 215);
-    body[PLUTO]->atmosphereHeight = 7;
-
-
-    craft.push_back ( new habitat() );
-
-    craft[HAB]->name = "Habitat";
-    craft[HAB]->fillColour = makecol (211, 211, 211);
-    craft[HAB]->engineColour = makecol (139, 0, 0);
-    craft[HAB]->radius = 30;
-    craft[HAB]->Vx = 0;
-    craft[HAB]->Vy = 0;
-    craft[HAB]->x = 0;
-    craft[HAB]->y = 0;
-    craft[HAB]->mass = 5e3;
-    craft[HAB]->turnRadians = 0;
-    craft[HAB]->turnRate = 0;
-    craft[HAB]->engineRadius = 8;
-
-    camera.zoom = 0;
-    camera.x = craft[HAB]->x - (SCREEN_W / 4);
-    camera.y = craft[HAB]->y - (SCREEN_H / 4);
-=======
 	//allegro initializations
 	allegro_init();
 	install_keyboard();
@@ -577,7 +399,6 @@ int main () {
 	camera.zoomLevel = 0;
 	camera.x = craft[HAB]->x - (SCREEN_W / 4);
 	camera.y = craft[HAB]->y - (SCREEN_H / 4);
->>>>>>> 303d7b31551601a273243cfe312ef018f4fe2d71
 
 //	camera.target = craft[HAB];
 	camera.target = body[EARTH];
@@ -770,48 +591,17 @@ long int entity::distance (long double targetX, long double targetY) { //finds d
 
 long int entity::a() { //on-screen x position of entity
 
-<<<<<<< HEAD
-    circlefill (buffer, a(), b(), radius * camera.actualZoom(), fillColour); //draws the picture to the buffer
-    line (buffer, a(), b(), //draws the 'engine'
-          a() + radius * cos (turnRadians) * camera.actualZoom(),
-          b() + radius * sin (turnRadians) * camera.actualZoom(),
-          engineColour);
-=======
 	return ( (x - camera.x) * camera.actualZoom() + SCREEN_W / 2);
 }
 
 long int entity::b() { //on-screen y position of entity
 
 	return ( (y - camera.y) * camera.actualZoom() + SCREEN_H / 2);
->>>>>>> 303d7b31551601a273243cfe312ef018f4fe2d71
 }
 
 void entity::draw() {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    circlefill (buffer, a(), b(), radius * camera.actualZoom(), fillColour); //draws the picture to the buffer
-    circlefill (buffer, //draws the center 'engine'
-                a() + (radius - engineRadius * camera.actualZoom() / 2) * cos (turnRadians - PI) * camera.actualZoom(),
-                b() + (radius - engineRadius * camera.actualZoom() / 2) * sin (turnRadians - PI) * camera.actualZoom(),
-                engineRadius * camera.actualZoom(),
-                engineColour);
-    circlefill (buffer, //draws the left 'engine'
-                a() + radius * cos (turnRadians - (PI * .75) ) * camera.actualZoom(),
-                b() + radius * sin (turnRadians - (PI * .75) ) * camera.actualZoom(),
-                engineRadius * camera.actualZoom(),
-                engineColour);
-    circlefill (buffer, //draws the right 'engine'
-                a() + radius * cos (turnRadians - (PI * 1.25) ) * camera.actualZoom(),
-                b() + radius * sin (turnRadians - (PI * 1.25) ) * camera.actualZoom(),
-                engineRadius * camera.actualZoom(),
-                engineColour);
-=======
-	circlefill (buffer, a(), b(), radius * camera.zoom, fillColour); //draws the entity to the buffer
->>>>>>> 303d7b31551601a273243cfe312ef018f4fe2d71
-=======
 	circlefill (buffer, a(), b(), radius * camera.actualZoom(), fillColour); //draws the entity to the buffer
->>>>>>> 3ea610a51199a88bcb0e294b76e2f36285e09815
 }
 
 void solarBody::draw() {
@@ -825,9 +615,6 @@ void solarBody::draw() {
 
 void ship::draw() {
 
-<<<<<<< HEAD
-    return (pow (zoomMagnitude, zoom) + 0.0000000001);  //to avoid divide by zero errors
-=======
 	float A = a();  //so that the program doesn't have to calculate a and b every time
 	float B = b();
 
@@ -836,7 +623,6 @@ void ship::draw() {
 		  A + radius * cos (turnRadians) * camera.actualZoom(),
 		  B + radius * sin (turnRadians) * camera.actualZoom(),
 		  engineColour);
->>>>>>> 303d7b31551601a273243cfe312ef018f4fe2d71
 }
 
 void habitat::draw() {
