ced080d0a49ca9b7ca25961a1009be5daccd8e9e
Patrick Melanson <patrickpowns@gmail.com>
2012-06-06 22:41:50 -0400

    started on dumb vector iteration

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "05";
+	static const char DATE[] = "06";
 	static const char MONTH[] = "06";
 	static const char YEAR[] = "2012";
 	static const double UBUNTU_VERSION_STYLE = 12.06;
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 0;
-	static const long BUILD = 25;
-	static const long REVISION = 173;
+	static const long BUILD = 26;
+	static const long REVISION = 175;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 1833;
-	#define RC_FILEVERSION 1,0,25,173
-	#define RC_FILEVERSION_STRING "1, 0, 25, 173\0"
-	static const char FULLVERSION_STRING[] = "1.0.25.173";
+	static const long BUILDS_COUNT = 2093;
+	#define RC_FILEVERSION 1,0,26,175
+	#define RC_FILEVERSION_STRING "1, 0, 26, 175\0"
+	static const char FULLVERSION_STRING[] = "1.0.26.175";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 7;
+	static const long BUILD_HISTORY = 8;
 	
 
 }
