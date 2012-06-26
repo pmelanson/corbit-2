01c6db04ec794120588e21c56c1f4f9c7faaf58c
Patrick Melanson <patrickpowns@gmail.com>
2012-06-26 11:07:39 -0400

    I'll probably be working on this over the summer

---
+++ strings.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+
+	int poop = 13;
+	printf ("%d\n", poop);
+
+	printf ("%f\n", poop);
+
+	float pooper = float(poop);
+
+	printf ("%f\n", pooper);
+}
