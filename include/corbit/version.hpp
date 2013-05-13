3f3ba9cdfb1b50f57f5442d8a2742f2347dad770
patrickpowns <patrick.melanstone@gmail.com>
2013-05-13 13:47:43 -0400

    cleaning up directory structure

--- include/corbit/version.hpp
+++ include/corbit/version.hpp
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "24";
-	static const char MONTH[] = "12";
-	static const char YEAR[] = "2012";
-	static const char UBUNTU_VERSION_STYLE[] = "12.12";
+	static const char DATE[] = "13";
+	static const char MONTH[] = "05";
+	static const char YEAR[] = "2013";
+	static const char UBUNTU_VERSION_STYLE[] = "13.05";
 	
 	//Software Status
 	static const char STATUS[] = "Beta";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 2;
 	static const long MINOR = 0;
-	static const long BUILD = 9;
-	static const long REVISION = 35842;
+	static const long BUILD = 1;
+	static const long REVISION = 35849;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 3198;
-	#define RC_FILEVERSION 2,0,9,35842
-	#define RC_FILEVERSION_STRING "2, 0, 9, 35842\0"
-	static const char FULLVERSION_STRING[] = "2.0.9.35842";
+	static const long BUILDS_COUNT = 2;
+	#define RC_FILEVERSION 2,0,1,35849
+	#define RC_FILEVERSION_STRING "2, 0, 1, 35849\0"
+	static const char FULLVERSION_STRING[] = "2.0.1.35849";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 11;
+	static const long BUILD_HISTORY = 0;
 	
 
 }
