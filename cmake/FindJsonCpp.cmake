1c3da4154a4e790fee83f29df36b81a753865e5f
patrickpowns <patrick.melanstone@gmail.com>
2013-05-27 15:34:39 -0400

    got a better FindJsonCpp.cmake

--- cmake/FindJsonCpp.cmake
+++ cmake/FindJsonCpp.cmake
@@ -1,98 +1,55 @@
-# - try to find JSONCPP library
-#
-# Cache Variables: (probably not for direct use in your scripts)
-#  JSONCPP_INCLUDE_DIR
-#  JSONCPP_LIBRARY
-#
-# Non-cache variables you might use in your CMakeLists.txt:
-#  JSONCPP_FOUND
-#  JSONCPP_INCLUDE_DIRS
-#  JSONCPP_LIBRARIES
-#
-# Requires these CMake modules:
-#  FindPackageHandleStandardArgs (known included with CMake >=2.6.2)
-#
-# Author:
-# 2011 Philippe Crassous (ENSAM ParisTech / Institut Image) p.crassous _at_ free.fr
+# -*- cmake -*-
+
+# - Find JSONCpp
+# Find the JSONCpp includes and library
+# This module defines
+#  JSONCPP_INCLUDE_DIR, where to find json.h, etc.
+#  JSONCPP_LIBRARIES, the libraries needed to use jsoncpp.
+#  JSONCPP_FOUND, If false, do not try to use jsoncpp.
+#  also defined, but not for general use are
+#  JSONCPP_LIBRARY, where to find the jsoncpp library.
+
+FIND_PATH(JSONCPP_INCLUDE_DIR jsoncpp/json.h
+/usr/local/include
+/usr/include
+)
+
+# Get the GCC compiler version
+EXEC_PROGRAM(${CMAKE_CXX_COMPILER}
+            ARGS ${CMAKE_CXX_COMPILER_ARG1} -dumpversion
+            OUTPUT_VARIABLE _gcc_COMPILER_VERSION
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            )
+
+SET(JSONCPP_NAMES ${JSONCPP_NAMES} libjson_linux-gcc-${_gcc_COMPILER_VERSION}_libmt.so)
+FIND_LIBRARY(JSONCPP_LIBRARY
+  NAMES ${JSONCPP_NAMES}
+  PATHS /usr/lib /usr/local/lib
+  )
+
+IF (JSONCPP_LIBRARY AND JSONCPP_INCLUDE_DIR)
+    SET(JSONCPP_LIBRARIES ${JSONCPP_LIBRARY})
+    SET(JSONCPP_FOUND "YES")
+ELSE (JSONCPP_LIBRARY AND JSONCPP_INCLUDE_DIR)
+  SET(JSONCPP_FOUND "NO")
+ENDIF (JSONCPP_LIBRARY AND JSONCPP_INCLUDE_DIR)
 
 
-set(JSONCPP_ROOT_DIR
-	"${JSONCPP_ROOT_DIR}"
-	CACHE
-	PATH
-	"Directory to search for JSONCPP")
-set(_jsoncppnames)
-set(_pathsuffixes
-	suncc
-	vacpp
-	mingw
-	msvc6
-	msvc7
-	msvc71
-	msvc80
-	msvc90
-	linux-gcc)
-if(CMAKE_COMPILER_IS_GNUCXX)
-	execute_process(COMMAND
-		${CMAKE_CXX_COMPILER}
-		-dumpversion
-		OUTPUT_VARIABLE
-		_gnucxx_ver
-		OUTPUT_STRIP_TRAILING_WHITESPACE)
-	list(APPEND
-		_jsoncppnames
-		json_linux-gcc-${_gnucxx_ver}_libmt
-		json_linux-gcc_libmt)
-	list(APPEND _pathsuffixes linux-gcc-${_gnucxx_ver})
-elseif(MSVC)
-	if(MSVC_VERSION EQUAL 1200)
-		list(APPEND _jsoncppnames json_vc6_libmt)
-	elseif(MSVC_VERSION EQUAL 1300)
-		list(APPEND _jsoncppnames json_vc7_libmt)
-	elseif(MSVC_VERSION EQUAL 1310)
-		list(APPEND _jsoncppnames json_vc71_libmt)
-	elseif(MSVC_VERSION EQUAL 1400)
-		list(APPEND _jsoncppnames json_vc8_libmt)
-	elseif(MSVC_VERSION EQUAL 1500)
-		list(APPEND _jsoncppnames json_vc9_libmt)
-	elseif(MSVC_VERSION EQUAL 1600)
-		list(APPEND _jsoncppnames json_vc10_libmt)
-	endif()
-else()
-	list(APPEND _jsoncppnames
-		json_suncc_libmt
-		json_vacpp_libmt)
-endif()
+IF (JSONCPP_FOUND)
+   IF (NOT JSONCPP_FIND_QUIETLY)
+      MESSAGE(STATUS "Found JSONCpp: ${JSONCPP_LIBRARIES}")
+   ENDIF (NOT JSONCPP_FIND_QUIETLY)
+ELSE (JSONCPP_FOUND)
+   IF (JSONCPP_FIND_REQUIRED)
+      MESSAGE(FATAL_ERROR "Could not find JSONCpp library")
+   ENDIF (JSONCPP_FIND_REQUIRED)
+ENDIF (JSONCPP_FOUND)
 
-list(APPEND _jsoncppnames
-	json_mingw_libmt)
+# Deprecated declarations.
+SET (NATIVE_JSONCPP_INCLUDE_PATH ${JSONCPP_INCLUDE_DIR} )
+GET_FILENAME_COMPONENT (NATIVE_JSONCPP_LIB_PATH ${JSONCPP_LIBRARY} PATH)
 
-find_library(JSONCPP_LIBRARY
-	NAMES
-	${_jsoncppnames}
-	PATHS
-	"${JSONCPP_ROOT_DIR}/libs"
-	PATH_SUFFIXES
-	${_pathsuffixes})
-
-find_path(JSONCPP_INCLUDE_DIR
-	NAMES
-	json/json.h
-	PATHS
-	"${JSONCPP_ROOT_DIR}"
-	PATH_SUFFIXES
-	include)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(JSONCPP
-	DEFAULT_MSG
-	JSONCPP_LIBRARY
-	JSONCPP_INCLUDE_DIR)
-
-if(JSONCPP_FOUND)
-	set(JSONCPP_LIBRARIES "${JSONCPP_LIBRARY}")
-	set(JSONCPP_INCLUDE_DIRS "${JSONCPP_INCLUDE_DIR}")
-	mark_as_advanced(JSONCPP_ROOT_DIR)
-endif()
-
-mark_as_advanced(JSONCPP_INCLUDE_DIR JSONCPP_LIBRARY)
+MARK_AS_ADVANCED(
+  JSONCPP_LIBRARY
+  JSONCPP_INCLUDE_DIR
+  )
